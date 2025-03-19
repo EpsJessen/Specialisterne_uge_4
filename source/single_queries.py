70e3abcf8016a4e976b8cb03f4bbd8657f6b949c
Epsilon <epsijes@gmail.com>
2025-03-19 15:37:11 +0100

    Update single_queries.py
    Function for adding new orders

--- source/single_queries.py
+++ source/single_queries.py
@@ -11,6 +11,31 @@ class QueryMaker:
         db.make_populated_table("Orders_combined", "orders_combined.csv")
         self._connector = Connector()
 
+    #CREATE QUERIES
+    def new_order(self, customer_name:str, customer_email:str,
+                    product_name:str, product_price:float,
+                    time:datetime.datetime|None = None):
+        
+        if not time:
+            time = datetime.datetime.now()
+        #Gets the number of sales. ID of sales starts at 0 <---IDs SHOULD start at 1
+        #Thus the next ID will be the same as the number of previous orders
+        next_id = self.nr_sales()[0][0][0]
+        
+        query = f"""
+                    INSERT INTO Orders_combined (
+                        ID, date_time, `Customer Name`, `Customer email`,
+                        `Product Name`, `Product Price`
+                    )
+                    VALUES (
+                        {next_id}, '{time}','{customer_name}', '{customer_email}',
+                        '{product_name}', {product_price}
+                    )
+                """
+        self._connector.executeCUD(query)
+
+    
+
     #READ QUERIES
     def select_all(self):
         query = "SELECT * FROM Orders_combined"
@@ -48,8 +73,10 @@ class QueryMaker:
     
 def main():
     qm = QueryMaker()
-    qm.printR(qm.nr_sales())
-    #qm.print_all()
+    #qm.printR(qm.nr_sales_by_product())
+    
+    qm.new_order("Epsilon","not_my@email.com","yoga-mat",100)
+    qm.print_all()
     
 
 
