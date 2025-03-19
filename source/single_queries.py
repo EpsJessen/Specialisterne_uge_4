5a5c381b4951948bd9996aab4262c8541592f219
Epsilon <epsijes@gmail.com>
2025-03-19 14:05:52 +0100

    Update single_queries.py
    Initialize with connection
    (Should be refactored later so it doesn't create the DB)

--- source/single_queries.py
+++ source/single_queries.py
@@ -4,8 +4,20 @@ import datetime
 from tabulate import tabulate
 
 class QueryMaker:
+    _connector:Connector
+
+    def __init__(self):
+        db = DBFromCsv()
+        db.make_populated_table("Orders_combined", "orders_combined.csv")
+        self._connector = Connector()
+
     
 def main():
+    qm = QueryMaker()
+    qm.printR(qm.spenders_after("2025-10-01"))
+    qm.print_all()
     
+
+
 if __name__ == "__main__":
     main()
