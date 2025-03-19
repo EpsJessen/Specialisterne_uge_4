287cd4144e057ddcd50c0d5757e4ac4c49ae960d
Epsilon <epsijes@gmail.com>
2025-03-19 11:23:21 +0100

    Update db_from_csv.py
    Function for populating predefined table with values from csv file

--- source/db_from_csv.py
+++ source/db_from_csv.py
@@ -27,3 +27,23 @@ class DBFromCsv:
         self._connector.executeCUD(f"DROP TABLE IF EXISTS {table_name}")
         self._connector.executeCUD(create_table_stmnt)
 
+    def populate_table(self, table_name:str, csv_file:str):
+        os_path = join("data", csv_file)
+
+        sql_insert = """INSERT INTO %s (
+                            ID, time, `Customer Name`, `Customer Email`,
+                            `Product Name`, `Product Price`)
+                            VALUES (%s, '%s', '%s', '%s', '%s', %s)"""
+        
+        with open(os_path, "r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            _ = next(csv_reader)
+
+            for row in csv_reader:
+                row = [table_name] + row
+                row[2].replace("T", " ")
+                self._connector.executeCUD(sql_insert % tuple(row))
+        
+
+        
+
