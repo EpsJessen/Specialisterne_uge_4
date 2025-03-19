c00a2b4062669d092a6b9265cacbea5bd84c31e7
Epsilon <epsijes@gmail.com>
2025-03-19 14:04:33 +0100

    Create single_queries.py

---
+++ source/single_queries.py
@@ -0,0 +1,11 @@
+from db_from_csv import DBFromCsv
+from connector import Connector
+import datetime
+from tabulate import tabulate
+
+class QueryMaker:
+    
+def main():
+    
+if __name__ == "__main__":
+    main()
