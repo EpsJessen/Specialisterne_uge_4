3b6eac937b18b57a4a3fad84b0fa95b44ac9a20e
Epsilon <epsijes@gmail.com>
2025-03-19 11:22:36 +0100

    Update db_from_csv.py
    Function for adding a single predefined table to db

--- source/db_from_csv.py
+++ source/db_from_csv.py
@@ -12,3 +12,18 @@ class DBFromCsv:
         # settings defined by Connector
         self._connector = Connector(exists=False)
 
+    def add_table(self, table_name:str) -> None:
+        fields = """ID int NOT NULL,
+                    time DATETIME,
+                    `Customer Name` VARCHAR(250) NOT NULL,
+                    `Customer Email` VARCHAR(250) NOT NULL,
+                    `Product Name` VARCHAR(100) NOT NULL,
+                    `Product Price` float NOT NULL,
+                    PRIMARY KEY (ID)
+                    """
+
+        create_table_stmnt = f"CREATE TABLE {table_name}({fields});"
+
+        self._connector.executeCUD(f"DROP TABLE IF EXISTS {table_name}")
+        self._connector.executeCUD(create_table_stmnt)
+
