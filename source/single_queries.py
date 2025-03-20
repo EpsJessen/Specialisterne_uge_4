5be799a4c6ef85b1c047b4f4670fd09dac7192e2
Epsilon <epsijes@gmail.com>
2025-03-20 17:45:34 +0100

    Update single_queries.py
    Comments

--- source/single_queries.py
+++ source/single_queries.py
@@ -12,6 +12,7 @@ class QueryMaker:
         self._connector = Connector()
 
     #CREATE QUERIES
+    # Add a new order to Orders_combined db
     def new_order(self, customer_name:str, customer_email:str,
                     product_name:str, product_price:float,
                     time:datetime.datetime|None = None):
@@ -37,13 +38,16 @@ class QueryMaker:
     
 
     #READ QUERIES
+    # Get full orders_combined table
     def select_all(self):
         query = "SELECT * FROM Orders_combined"
         return self._connector.executeR(query)
     
+    # Pretty print full table
     def print_all(self):
         self.printR(self.select_all())
     
+    # Spenders ordered by sum of money spent after given time
     def spenders_after(self, date:str):
         query = f"""SELECT CN AS Customer, SUM(PP) AS `Total Spending`
                     FROM (
@@ -55,6 +59,7 @@ class QueryMaker:
                     ORDER BY `Total Spending` DESC"""
         return self._connector.executeR(query)
     
+    # Products sorted by amount sold
     def nr_sales_by_product(self):
         query = f"""
                     SELECT `Product Name` AS N, COUNT(`Product Price`) as Revenue
@@ -64,10 +69,13 @@ class QueryMaker:
                 """
         return self._connector.executeR(query)
     
+    # Total number of sales
     def nr_sales(self):
         query = f"SELECT COUNT(ID) FROM Orders_combined"
         return self._connector.executeR(query)
     
+    # Helper function to get the biggest id in table
+    # Returns -1 if no items in table
     def _max_ID(self):
         query = "SELECT MAX(ID) FROM Orders_combined"
         res, _ = self._connector.executeR(query)
@@ -75,10 +83,12 @@ class QueryMaker:
             return -1
         return res[0][0]
     
+    # Pretty print for retur of queries
     def printR(self, read_result):
         print(tabulate(read_result[0], read_result[1]))
 
     # UPDATE QUERIES
+    # Update value in column where ID matches
     def update_column_where_id(self, column_name, target_id, new_val):
         if type(new_val) == str and " " in new_val:
             new_val = f"'{new_val}'"
@@ -88,8 +98,10 @@ class QueryMaker:
                     WHERE ID = {target_id}
                 """
         self._connector.executeCUD(query)
-        
-    def update_column_where_it_has_value(self, column_name, current_val, new_val):
+    
+    # Update value in column where the value of the column matches
+    def update_column_where_it_has_value(self, column_name,
+                                         current_val, new_val):
         if type(current_val) == str and " " in current_val:
             current_val = f"'{current_val}'"
         if type(new_val) == str and " " in new_val:
@@ -105,6 +117,7 @@ class QueryMaker:
             print("Incorrect arguments for query!")
     
     # DELETE QUERIES
+    # Delete every row where the value of given column matches 
     def delete_rows_where_column_value(self, column_name, current_val):
         if type(current_val) == str and " " in current_val:
             current_val = f"'{current_val}'"
@@ -116,10 +129,12 @@ class QueryMaker:
             self._connector.executeCUD(query)
         except:
             print("Incorrect arguments for query!")
-    
+
+    # Delete row where id matches
     def delete_row_where_id(self, target_id):
         self.delete_rows_where_column_value("ID", target_id)
 
+    # Delete rows with date_time is older than given time
     def delete_rows_from_before(self, timestamp:datetime.datetime):
         query = f"""
                     DELETE FROM Orders_combined
@@ -130,6 +145,7 @@ class QueryMaker:
         except:
             print("Incorrect arguments for query!")
 
+    # Delete rows with date_time is newer than given time
     def delete_rows_from_after(self, timestamp:datetime.datetime) -> None:
         query = f"""
                     DELETE FROM Orders_combined
