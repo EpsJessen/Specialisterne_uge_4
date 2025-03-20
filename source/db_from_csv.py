289cb0491a87fa2a644d4a0c2976b6c84393cec0
Epsilon <epsijes@gmail.com>
2025-03-20 17:17:58 +0100

    Update db_from_csv.py
    Refectorisation to infer table

--- source/db_from_csv.py
+++ source/db_from_csv.py
@@ -13,41 +13,42 @@ class DBFromCsv:
         # settings defined by Connector
         self._connector = Connector(exists=False)
 
-    def add_table(self, table_name:str) -> None:
-        fields = """ID int NOT NULL,
-                    date_time DATETIME,
-                    `Customer Name` VARCHAR(250) NOT NULL,
-                    `Customer Email` VARCHAR(250) NOT NULL,
-                    `Product Name` VARCHAR(100) NOT NULL,
-                    `Product Price` float NOT NULL,
-                    PRIMARY KEY (ID)
-                    """
+    def add_table(self, table_name:str, headers, row, pk:str = "ID") -> None:
+        fields = ""
+        for i, name in enumerate(headers):
+            fields += f"`{name}` {self.item_to_sql_type(row[i])} NOT NULL,"
+        fields += f"PRIMARY KEY (`{pk}`)"
 
         create_table_stmnt = f"CREATE TABLE {table_name}({fields});"
 
         self._connector.executeCUD(f"DROP TABLE IF EXISTS {table_name}")
         self._connector.executeCUD(create_table_stmnt)
 
-    def populate_table(self, table_name:str, csv_file:str):
-        os_path = join("data", csv_file)
-
-        sql_insert = """INSERT INTO %s (
-                            ID, date_time, `Customer Name`, `Customer Email`,
-                            `Product Name`, `Product Price`)
-                            VALUES (%s, '%s', '%s', '%s', '%s', %s)"""
+    def populate_table(self, table_name:str, headers, rows):
+        sql_insert = "INSERT INTO %s ("
         
+        for header in headers:
+            sql_insert += f"`{header}`, "
+        sql_insert = sql_insert[:-2]
+        #DETERMINE WHETHER EACH VALUE SHOULD BE EXPLICITLY MARKED AS STRING
+        sql_insert += f") VALUES ({self.stringy_substitude(rows[0])})"
+        
+        datetime_columns = self.datetime_columns(rows[0])
+        for row in rows:
+            #CONVERT DATETIMES TO PROPER FORMAT
+            row = [table_name] + row
+            for i in datetime_columns:
+                row[i+1]=datetime.fromisoformat(row[i+1])
+            self._connector.executeCUD(sql_insert % tuple(row))
+        
+    def make_populated_table(self, table_name, file_name):
+        os_path = join("data", file_name)
         with open(os_path, "r") as csv_file:
             csv_reader = csv.reader(csv_file)
-            _ = next(csv_reader)
-
-            for row in csv_reader:
-                row = [table_name] + row
-                row[2].replace("T", " ")
-                self._connector.executeCUD(sql_insert % tuple(row))
-        
-    def make_populated_table(self, table_name, csv_file):
-        self.add_table(table_name)
-        self.populate_table(table_name, csv_file)
+            headers = next(csv_reader)
+            rows = list(csv_reader)
+        self.add_table(table_name, headers, rows[0])
+        self.populate_table(table_name, headers, rows)
 
     def try_convert(self, input):
         try:
@@ -63,6 +64,24 @@ class DBFromCsv:
             return float(input)
         except:
             return input
+        
+    def datetime_columns(self, row):
+        columns = []
+        for i, item in enumerate(row):
+            type_item = self.try_convert(item)
+            if isinstance(type_item, datetime):
+                columns.append(i)
+        return columns
+    
+    def stringy_substitude(self, row):
+        substitudes = ""
+        for item in row:
+            type_item = self.try_convert(item)
+            if isinstance(type_item, (int, float)):
+                substitudes += "%s, "
+            else:
+                substitudes += "'%s', "
+        return substitudes[:-2]
 
     def item_to_sql_type(self, item) -> str:
         type_item = self.try_convert(item)
@@ -75,9 +94,6 @@ class DBFromCsv:
         else:
             return "VARCHAR(250)"
 
-
-
-    
 def main():
     db = DBFromCsv()
     db.make_populated_table("Orders_combined", "orders_combined.csv")
