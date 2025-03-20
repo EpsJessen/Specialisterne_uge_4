9b0d646fa963d6e9290b51c986b8670da64a44b7
Epsilon <epsijes@gmail.com>
2025-03-20 12:43:20 +0100

    Update db_from_csv.py
    Determine relevant types of csv datapoints

--- source/db_from_csv.py
+++ source/db_from_csv.py
@@ -1,6 +1,7 @@
 from connector import Connector
 from os.path import join
 import csv
+from datetime import datetime
 
 # Class for creating a database from a csv file using an instance of the
 # Connector class
@@ -48,10 +49,32 @@ class DBFromCsv:
         self.add_table(table_name)
         self.populate_table(table_name, csv_file)
 
+    def try_convert(self, input):
+        try:
+            dt_obj = datetime.fromisoformat(input)
+            return dt_obj
+        except:
+            pass
+        try:
+            return int(input)
+        except:
+            pass
+        try:
+            return float(input)
+        except:
+            return input
+
     
 def main():
     db = DBFromCsv()
     db.make_populated_table("Orders_combined", "orders_combined.csv")
+    os_path = join("data", "orders_combined.csv")
+    with open(os_path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        _ = next(csv_reader)
+        vals = next(csv_reader)
+        for val in vals:
+            print(f"{val} is of type {type(db.try_convert(val))}")
 
 if __name__ == "__main__":
     main()
