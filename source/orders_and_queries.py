9770c889b8120a76c5631de15551d80477464419
Epsilon <epsijes@gmail.com>
2025-03-21 11:10:23 +0100

    Create orders_and_queries.py
    create and connect to db

---
+++ source/orders_and_queries.py
@@ -0,0 +1,29 @@
+from db_from_csv import DBFromCsv
+from single_queries import SingleQueryMaker
+from multi_queries import MultiQueryMaker
+
+
+def populate_db() -> None:
+    db = DBFromCsv()
+    db.make_populated_table("Orders_combined", "orders_combined.csv")
+    db.make_populated_tables(
+        ["customers", "products", "orders"],
+        ["customers.csv", "products.csv", "orders.csv"],
+        [
+            None,
+            None,
+            [
+                {"table": "customers", "key": "id", "fk": "customer"},
+                {"table": "products", "key": "id", "fk": "product"},
+            ],
+        ],
+    )
+
+
+def main():
+    populate_db()
+    single = SingleQueryMaker()
+    relational = MultiQueryMaker()
+
+if __name__ == "__main__":
+    main()
