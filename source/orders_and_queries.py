17f4083fb5f4385fdd3f8b3afafb3164f8947eed
Epsilon <epsijes@gmail.com>
2025-03-21 14:21:47 +0100

    Update orders_and_queries.py
    Sample queries

--- source/orders_and_queries.py
+++ source/orders_and_queries.py
@@ -19,11 +19,58 @@ def populate_db() -> None:
         ],
     )
 
+def pause() -> None:
+    _ = input("Press enter to continue")
 
 def main():
     populate_db()
     single = SingleQueryMaker()
     relational = MultiQueryMaker()
+    #SHOW EACH QUERY, WAIT FOR INPUT TO PERFORM AND SHOW NEXT
+    #print("Read all orders combined")
+    #pause()
+    #relational.print_all()
+    #pause()
+    #print("Read biggest spenders after right now")
+    #pause()
+    #relational.printR(relational.spenders_after())
+    #pause()
+    #print("Read products by amount sold")
+    #pause()
+    #relational.printR(relational.nr_sales_by_product())
+    #pause()
+    #print("Read total number of sales")
+    #pause()
+    #relational.printR(relational.nr_sales())
+    #pause()
+    #print("Create new product, Dock, and print all products after")
+    relational.new_product("Dock", 119.95)
+    #pause()
+    #relational.printR(relational.get_table("Products"))
+    #print("Create new customer Epsilon, and print all customers after")
+    relational.new_customer("Epsilon", "not@my.email")
+    #pause()
+    #relational.printR(relational.get_table("Customers"))
+    #pause()
+    #print("Create new order, then print last 10 orders combined")
+    relational.new_order(30, 10)
+    #pause()
+    #relational.print_all(limit=10, offset=91)
+    #pause()
+    print("Update order 99 to also be of Dock, then print last ten orders")
+    relational.update_row_where_id("Orders", "product", 99, 10)
+    #pause()
+    #relational.print_all(limit=10, offset=91)
+    #pause()
+    print("Change the name of everyone who bougth a mouse to Rat King,",
+    " then print all customers")
+    relational.update_whole_schema("Products", "Customers", "name", "name", "mouse", "Rat King")
+    pause()
+    relational.printR(relational.get_table("Customers"))
+
+
+
+
 
 
 if __name__ == "__main__":
