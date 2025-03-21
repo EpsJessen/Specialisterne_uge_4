11c07852d0d20f641db488adc0074342352ac230
Epsilon <epsijes@gmail.com>
2025-03-21 14:23:02 +0100

    Update multi_queries.py
    Bug huntin'

--- source/multi_queries.py
+++ source/multi_queries.py
@@ -66,7 +66,7 @@ class MultiQueryMaker:
             print(f"Could not perform {query=}")
 
     # Adds new customer
-    def new_product(self, name: str, email: str):
+    def new_customer(self, name: str, email: str):
         try:
             next_id = self._max_ID("customers") + 1
         except:
@@ -74,7 +74,7 @@ class MultiQueryMaker:
             return
 
         query = f"""
-                    INSERT INTO Products (
+                    INSERT INTO Customers (
                         ID, name, email
                     )
                     VALUES (
@@ -131,7 +131,7 @@ class MultiQueryMaker:
 
     # Total number of sales
     def nr_sales(self):
-        query = f"SELECT COUNT(ID) FROM Orders"
+        query = f"SELECT COUNT(ID) AS `Items sold` FROM Orders"
         return self._connector.executeR(query)
 
     # Helper function to get the biggest id in table
@@ -148,10 +148,28 @@ class MultiQueryMaker:
         print(tabulate(read_result[0], read_result[1]))
 
     # UPDATE QUERIES
-    # Update value in column where ID matches
-    def update_column_where_id(self, column_name, target_id, new_val):
+    # Update any column in orders combined based on any other
+    def update_whole_schema(self,
+                            reason_table, target_table,
+                            reason_column, target_column,
+                            reason_val, new_val):
+        if not isinstance(reason_val, (int, float)):
+            reason_val = f"'{reason_val}'"
+        if not isinstance(new_val, (int, float)):
+            new_val = f"'{new_val}'"
         query = f"""
-                    UPDATE Orders_combined
+                    UPDATE orders
+                    INNER JOIN products ON orders.product = products.id
+                    Inner join customers on orders.customer = customers.id
+                    SET `{target_table}`.`{target_column}` = {new_val}
+                    WHERE `{reason_table}`.`{reason_column}` = {reason_val};
+                """
+        self._connector.executeCUD(query)
+
+    # Update item from column where ID matches in table
+    def update_row_where_id(self, table, column_name, target_id, new_val):
+        query = f"""
+                    UPDATE {table}
                     SET `{column_name}` = '{new_val}'
                     WHERE ID = {target_id}
                 """
